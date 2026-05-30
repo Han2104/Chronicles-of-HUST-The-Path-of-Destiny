17441f30fbcff08dd98fcac47d9f9d2166a6a49b
Huynh Thanh Tuan <thanhtuann230205@gmail.com>
2026-05-30 18:36:55 +0700

    test

---
+++ target/classes/Map/C2/c2map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="c2map" tilewidth="16" tileheight="16" tilecount="6144" columns="96">
+ <image source="c2map.png" width="1536" height="1024"/>
+</tileset>
